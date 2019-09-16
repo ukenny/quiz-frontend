43e22a07d2ebcef1f3aca6d2e9b59a816bf23699
Kenny Urena <urenakenny@gmail.com>
2019-09-16 11:58:22 -0400

    Modified QuizComponent and made it reachable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { QuestionComponent } from './question/question.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { HomeComponent } from './home/home.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'question', component: QuestionComponent},
   {path: 'questions', component: QuestionsComponent},
+  {path: 'quiz', component: QuizComponent}
 ];
 
 @NgModule({
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-quiz',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  quiz = {};
+
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
   }
