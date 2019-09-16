338b783623f3e2d483514d04f09e57c50299adb9
Kenny Urena <urenakenny@gmail.com>
2019-09-16 07:37:26 -0400

    Added quiz and nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -14,17 +15,22 @@ import { MatCardModule } from '@angular/material/card';
 import { QuestionComponent } from './question/question.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { QuizComponent } from './quiz/quiz.component';
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
     QuestionsComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     AppRoutingModule,
     FormsModule,
     MatButtonModule,
